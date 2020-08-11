2ec07bea4de04bf04db29c04a9a3649e4c343dd4
butterfly <liuyonggang-jj@qq.com>
2020-08-11 16:47:19 +0800

    add miss file

---
+++ api/simple_client_socket/Common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define _SC_BEGIN	namespace sc {
+#define _SC_END		}
+#define _SC	::sc::
---
+++ api/simple_server_socket/Common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define _SS_BEGIN	namespace ss {
+#define _SS_END		}
+#define _SS	::ss::
