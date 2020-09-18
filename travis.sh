99f35bbc664389bd5fc0103a6f388fa4cdbfdcfa
butterfly <liuyonggang-jj@qq.com>
2020-09-18 16:30:35 +0800

    travis

--- travis.sh
+++ travis.sh
@@ -1,3 +1,24 @@
-cmake
+# build glog
+cd ..
+git clone -b v0.3.4 https://github.com/google/glog.git
+cd glog
+./configure
+make
+make install
+
+#build simple_server_socket
+cd ../simple_socket
+cd api/simple_server_socket
+cp Makefile_first Makefile
+make
+cp Makefile_second Makefile
+make
+make install
+
+#build simple_server_socket
+cd ../simple_client_socket
+cp Makefile_first Makefile
+make
+cp Makefile_second Makefile
 make
 make install
