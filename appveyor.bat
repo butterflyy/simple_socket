30c41ce9b5874ce7f71fec3b32eb7ec9b8ab0315
butterfly <liuyonggang-jj@qq.com>
2020-09-21 10:47:43 +0800

    update appveyor

---
+++ appveyor.bat
@@ -0,0 +1,19 @@
+echo "%cd%"
+cd ..
+git clone -b develop https://github.com/butterflyy/externals.git  --single-branch --depth=1
+
+echo "Build glog"
+cd externals
+git clone -b v0.3.4 https://github.com/google/glog.git --single-branch --depth=1
+cd glog
+msbuild google-glog.sln
+cd ..
+rename glog glog-0.3.4
+
+echo "Build gtest"
+git clone -b release-1.10.0 https://github.com/google/googletest.git  --single-branch --depth=1
+cd googletest
+cmake CMakeLists.txt
+msbuild googletest-distribution.sln
+cd ..
+rename googletest googletest-release-1.10.0
